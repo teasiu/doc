6e769d9ec8ef58bda93f3386e193ad18b1eb7bc4
teasiu <teasiu@qq.com>
2022-09-18 04:31:54 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/teasiu/doc/blob/main/docs/',
+            'https://github.com/teasiu/doc/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/teasiu/doc/blob/main/blog/',
+            'https://github.com/teasiu/doc/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
