d22a12790a18fda2d79bc3a8042bec03b70af875
teasiu <teasiu@qq.com>
2022-09-18 02:45:51 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '海思机顶盒使用教程',
+  title: '海思机顶盒 NAS 教程',
   tagline: '一个极低功耗的 Linux 服务器',
   url: 'https://doc.ecoo.top',
   baseUrl: '/',
