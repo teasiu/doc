ff173cd751bf5741751aae1b15c124a4a7dd16ad
teasiu <teasiu@qq.com>
2023-12-09 03:04:15 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-		<img src="img/logo.png" alt="Logo" />
+		<img src={require('./img/logo.svg').default} alt="Logo" />
 		<h1 className="hero__title">{siteConfig.title}</h1>
 		<p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
